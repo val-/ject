79838e49d6762608d1a791980e3cd0e1bb47cbaa
val- <webkoder@ya.ru>
2015-06-18 15:13:17 +0300

    refactor

--- src/js/app.js
+++ src/js/app.js
@@ -7,26 +7,20 @@
 /*jslint browser: true, nomen: true */
 /*global require, module, console, $, _, Promise*/
 
-(function () {
+window._ = require('underscore');
+if (!window.$) {
+    window.$ = require('zepto-browserify').$;
+}
+
+(function (window) {
     
     'use strict';
-    
-    var _ = require('underscore'),
-        names = ['Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent'],
-        otherNames = ['Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen'];
+            
+    $(function () {
 
-    function findSuperman(values) {
-        _.find(values, function (name) {
-            if (name === 'Clark Kent') {
-                console.log('It\'s Superman!');
-            } else {
-                console.log('... No superman!');
-            }
-        });
-    }
-    
-    _.each([names, otherNames], function (nameGroup) {
-        findSuperman(nameGroup);
+        $("#newJectPlaceholder button").text("Get started");
+
     });
 
-}());
+}(window));
+
